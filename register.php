461438e2b6c357928f6bd74f5430e67ccd308926
Nghongzhan <hongzhanng@yahoo.com.my>
2023-10-11 17:50:03 +0800

    upload picture

--- register.php
+++ register.php
@@ -19,7 +19,7 @@ include "dbConfig.php";
 
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
 
     <!-- Font Awesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
@@ -33,10 +33,10 @@ include "dbConfig.php";
 
     <style>
     body {
-      background-image: url('img/tourismBackground.jpg');
-      background-repeat: no-repeat;
+        background-image: url('img/tourismBackground.jpg');
+        background-repeat: no-repeat;
     }
-    </style> 
+    </style>
 </head>
 
 
@@ -55,54 +55,66 @@ include "dbConfig.php";
         </div>
     </div>
 
-    <div class="row align-items-center" style ="padding-top: 2%;padding-bottom: 2%;">
+    <div class="row align-items-center" style="padding-top: 2%;padding-bottom: 2%;">
         <div class="col-3"></div>
         <div class="col-6 bg-white rounded" style="padding: 30px 30px 30px 30px;">
-            <form method="POST" id="registerForm" action="register.php">
+            <form method="POST" id="registerForm" action="register.php" enctype="multipart/form-data">
                 <H1>Register Account</H1>
                 <div style="padding-top: 30px;"></div>
                 <div class="form-group form-inline">
-                    <label for="position" class="col-lg-2 text-right" style="justify-content: flex-end;">User Type:</label>
+                    <label for="position" class="col-lg-2 text-right" style="justify-content: flex-end;">User
+                        Type:</label>
                     <input type="Radio" id="S1" name="position" value="User">
                     <label for="S1" class="pl-2" style="padding-right:20px">User</label>
                     <input type="Radio" id="S2" name="position" value="Marchant">
                     <label for="S2" class="pl-2">Marchant</label>
-                  </div>
-                  <div class="form-group form-inline">
-                    <label for="username" class="col-lg-2 text-right" style="justify-content: flex-end;">Username:</label>
-                    <input type="text" class="form-control sizing col-lg-10" id="username" name="username" placeholder="Enter username" required>
-                  </div>
-                  <div class="form-group form-inline">
-                    <label for="password" class="col-lg-2 text-right" style="justify-content: flex-end;">Password:</label>
-                    <input type="text" class="form-control sizing col-lg-10" id="password" name="password" placeholder="Enter password" required>
-                  </div>
-                  <div class="form-group form-inline">
-                    <label for="contactNum" class="col-lg-2 text-right" style="justify-content: flex-end;">Contact Number:</label>
-                    <input type="text" class="form-control sizing col-lg-10" id="contactNum" name="contactNum" placeholder="Enter contact number" required>
-                  </div>
-                  <div class="form-group form-inline">
+                </div>
+                <div class="form-group form-inline">
+                    <label for="username" class="col-lg-2 text-right"
+                        style="justify-content: flex-end;">Username:</label>
+                    <input type="text" class="form-control sizing col-lg-10" id="username" name="username"
+                        placeholder="Enter username" required>
+                </div>
+                <div class="form-group form-inline">
+                    <label for="password" class="col-lg-2 text-right"
+                        style="justify-content: flex-end;">Password:</label>
+                    <input type="text" class="form-control sizing col-lg-10" id="password" name="password"
+                        placeholder="Enter password" required>
+                </div>
+                <div class="form-group form-inline">
+                    <label for="contactNum" class="col-lg-2 text-right" style="justify-content: flex-end;">Contact
+                        Number:</label>
+                    <input type="text" class="form-control sizing col-lg-10" id="contactNum" name="contactNum"
+                        placeholder="Enter contact number" required>
+                </div>
+                <div class="form-group form-inline">
                     <label for="email" class="col-lg-2 text-right" style="justify-content: flex-end;">Email:</label>
-                    <input type="text" class="form-control sizing col-lg-10" id="email" name="email" placeholder="Enter email" required>
-                  </div>
-                  <div class="form-group form-inline">
-                    <label for="description" class="col-lg-2 text-right" style="justify-content: flex-end;">Company Description:</label>
-                    <textarea rows="5" cols="40" type="text" class="form-control sizing col-lg-10" id="description" name="description" required></textarea>
-                  </div>
-                  <div class="form-group form-inline">
-                    <label for="document" class="col-lg-2 text-right" style="justify-content: flex-end;">Document:</label>
-                    <input type="file" class="form-control sizing col-lg-10" id="document" name="document" placeholder="Enter password" required>
-                  </div>
-                
-                  <container class="form-inline">
+                    <input type="text" class="form-control sizing col-lg-10" id="email" name="email"
+                        placeholder="Enter email" required>
+                </div>
+                <div class="form-group form-inline">
+                    <label for="description" class="col-lg-2 text-right" style="justify-content: flex-end;">Company
+                        Description:</label>
+                    <textarea rows="5" cols="40" type="text" class="form-control sizing col-lg-10" id="description"
+                        name="description" required></textarea>
+                </div>
+                <div class="form-group form-inline">
+                    <label for="document" class="col-lg-2 text-right"
+                        style="justify-content: flex-end;">Document:</label>
+                    <input type="file" class="form-control sizing col-lg-10" id="document" name="document" required>
+                </div>
+
+                <container class="form-inline">
                     <div class="col-lg-2"></div>
                     <div class="col-lg-10" style="padding-left: 0px;">
-                      <button type="submit" class="btn btn-primary" name="register">Register Employee</button>
+                        <button type="submit" class="btn btn-primary" name="register">Register Employee</button>
                     </div>
-                  </container>
+                </container>
             </form>
         </div>
     </div>
 </body>
+
 </html>
 
 
@@ -113,12 +125,44 @@ if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == "POST") {
   $ContactNum = $_POST['contactNum'];
   $Email = $_POST['email'];
   $CompDe = $_POST['description'];
-  $Document = $_POST['document'];
+  //$Document = $_POST['document'];
   $Status = "Pending";
   $Position = $_POST['position'];
 
+  
+  $img_name = $_FILES['document']['name'];
+  $img_size = $_FILES['document']['size'];
+  $tmp_name = $_FILES['document']['tmp_name'];
+  $error = $_FILES['document']['error'];
+
+
   $checkEmpIDSql = "SELECT * FROM userdb WHERE Username = '$Username'";
   $checkEmailSql = "SELECT * FROM userdb WHERE Email = '$Email'";
+
+  if ($error === 0) {
+    if ($img_size > 125000) {
+        $em = "Sorry, your file is too large.";
+    
+    } else {
+        $img_ex = pathinfo($img_name, PATHINFO_EXTENSION);
+        $img_ex_lc = strtolower($img_ex);
+        $allowed_exs = array("jpg", "jpeg", "png");
+
+        if (in_array($img_ex_lc, $allowed_exs)) {
+          $new_img_name = uniqid("IMG-", true) . '.' . $img_ex_lc;
+          $img_upload_path = 'verification_form/' . $new_img_name;
+
+          move_uploaded_file($tmp_name, $img_upload_path);
+   
+        } else {
+          $em = "You can't upload files of this type";
+  
+        }
+    }
+  } else {
+      $em = "unknown error occurred!";
+      header("Location: register.php?error=$em");
+  }
 
   if ($con->query($checkEmpIDSql)->num_rows > 0) {
     echo "<script>alert('Employee already exists!')</script>";
@@ -127,8 +171,9 @@ if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == "POST") {
     echo "<script>alert('Email already exists!')</script>";
     return;
   } else {
+    echo"<script>alert('$new_img_name!')</script>";
       $insertSql = "INSERT INTO userdb (`UserID`, `Username`, `Password`, `ContactNum`, `Email`, `CompDe`, `Document`,`Status`,`Position`)
-        VALUES ('','$Username', '$Password','$ContactNum', '$Email', '$CompDe', '$Document','$Status','$Position')";
+        VALUES ('','$Username', '$Password','$ContactNum', '$Email', '$CompDe', '$new_img_name','$Status','$Position')";
     }
 
     if ($con->query($insertSql) == true) {
