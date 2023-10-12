1526aa0463712396ceaf656fd63cca16e91bcb5c
Limsonghaw <songhao0258@gmail.com>
2023-10-12 14:37:44 +0800

    third

--- editmanage.php
+++ editmanage.php
@@ -1,4 +1,6 @@
 <?php
+
+    session_start();
     if(!isset($_GET['id'])){
         //redirect to show page
         die('id not provied');
@@ -36,7 +38,7 @@
             rel="stylesheet">
 
         <!-- Font Awesome -->
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet">
 
         <!-- Libraries Stylesheet -->
         <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
@@ -44,62 +46,33 @@
 
         <!-- Customized Bootstrap Stylesheet -->
         <link href="css/style.css" rel="stylesheet">
-
+        <style>
+            .btn-secondary {
+            color: #fff!important;
+            background-color: #6c757d!important;
+            border-color: #6c757d!important;
+        }
+        </style>
     </head>
 
 <body>
-    <!-- Topbar Start -->
-    <div class="container-fluid bg-light pt-3 d-none d-lg-block">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-6 text-center text-lg-left mb-2 mb-lg-0">
-                    <div class="d-inline-flex align-items-center">
-                        <p><i class="fa fa-envelope mr-2"></i>info@example.com</p>
-                        <p class="text-body px-3">|</p>
-                        <p><i class="fa fa-phone-alt mr-2"></i>+012 345 6789</p>
-                    </div>
-                </div>
-                <div class="col-lg-6 text-center text-lg-right">
-                    <div class="d-inline-flex align-items-center">
-                        <a class="text-primary px-3" href="">
-                            <i class="fab fa-facebook-f"></i>
-                        </a>
-                        <a class="text-primary px-3" href="">
-                            <i class="fab fa-twitter"></i>
-                        </a>
-                        <a class="text-primary px-3" href="">
-                            <i class="fab fa-linkedin-in"></i>
-                        </a>
-                        <a class="text-primary px-3" href="">
-                            <i class="fab fa-instagram"></i>
-                        </a>
-                        <a class="text-primary pl-3" href="">
-                            <i class="fab fa-youtube"></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Topbar End -->
-
-
-    <!-- Navbar Start -->
-    <div class="container-fluid position-relative nav-bar p-0">
+        <!-- Navbar Start -->
+        <div class="container-fluid position-relative nav-bar" style="padding-top: 1pc;padding-bottom: 15px;">
         <div class="container-lg position-relative p-0 px-lg-3" style="z-index: 9;">
             <nav class="navbar navbar-expand-lg bg-light navbar-light shadow-lg py-3 py-lg-0 pl-3 pl-lg-5">
                 <a href="" class="navbar-brand">
-                    <h1 class="m-0 text-primary"><span class="text-dark">TRAVEL</span>ER</h1>
+                    <h1 class="m-0 text-primary"><span class="text-dark">Promo</span>Tourism</h1>
                 </a>
                 <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse justify-content-between px-3" id="navbarCollapse">
                     <div class="navbar-nav ml-auto py-0">
-                        <a href="index.html" class="nav-item nav-link">Home</a>
-                        <a href="about.html" class="nav-item nav-link">About</a>
+                        <a href="index.php" class="nav-item nav-link active">Home</a>
+                        <!--<a href="about.html" class="nav-item nav-link">About</a>-->
                         <a href="service.html" class="nav-item nav-link">Services</a>
-                        <a href="package.html" class="nav-item nav-link active">Manage Product</a>
+                        <a href="package.html" class="nav-item nav-link">Tour Packages</a>
+                        <!--
                         <div class="nav-item dropdown">
                             <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Pages</a>
                             <div class="dropdown-menu border-0 rounded-0 m-0">
@@ -110,14 +83,23 @@
                                 <a href="testimonial.html" class="dropdown-item">Testimonial</a>
                             </div>
                         </div>
-                        <a href="contact.html" class="nav-item nav-link">Contact</a>
+                    -->
+                        <?php
+                            $Username = $_SESSION['username'];
+
+                            if($Username == null){
+                                echo ("<a href='login.php' class='nav-item nav-link'>Login/Sign Up</a>"); 
+                            }
+                            else{
+                                echo ("<a href='logout.php' class='nav-item nav-link'><i class='fa-solid fa-user'></i>".$_SESSION['username']."</a>");
+                            }
+                        ?>
                     </div>
                 </div>
             </nav>
         </div>
     </div>
     <!-- Navbar End -->
-
 
     <!-- Header Start -->
     <div class="container-fluid page-header">
@@ -165,7 +147,8 @@
                 <input type="hidden" class="form-control" id="old_product_pic" name="old_product_pic" value="<?php echo $product_pic; ?>">
             </div>
             <img src="<?php echo "uploads_img/".$product_pic;?> " class="img-thumbnail w-25" alt="">
-            <div class="row offset-10">
+            <div class="offset-10">
+                <button type="button" class="btn btn-secondary" onclick="history.go(-1);">Back</button>
                 <input class="btn btn-success" type="submit" value="Update">
             </div>
         </form>
